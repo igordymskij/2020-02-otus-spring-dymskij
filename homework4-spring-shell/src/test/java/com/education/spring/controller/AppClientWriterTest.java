c81af648a9a89d58ff1073fb6ce21a25612ee8b2
Blicker777 <GitHub7>
2020-06-07 13:59:26 +0300

    Тесты

--- homework4-spring-shell/src/test/java/com/education/spring/controller/AppClientWriterTest.java
+++ homework4-spring-shell/src/test/java/com/education/spring/controller/AppClientWriterTest.java
@@ -1,32 +1,26 @@
 package com.education.spring.controller;
 
-import com.education.spring.data.AppFileReader;
-import com.education.spring.service.AppServiceModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
 import org.springframework.shell.Shell;
-import org.springframework.shell.jline.InteractiveShellApplicationRunner;
-import org.springframework.shell.jline.ScriptShellApplicationRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(properties = {
-        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
-        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"})
+
+@SpringBootTest
 public class AppClientWriterTest {
 
-    @MockBean
-    private AppServiceModel serviceModel;
+//    @MockBean
+//    public AppServiceModel serviceModel;
+//
+//    @MockBean
+//    private AppFileReader fileReader;
+//
+//    @MockBean
+//    private MessageSource messageSource;
 
-    @MockBean
-    private AppFileReader fileReader;
-
-    @MockBean
-    private MessageSource messageSource;
-
+    @Autowired
     private AppClientWriter appClientWriter;
 
     @Autowired
@@ -34,14 +28,18 @@ public class AppClientWriterTest {
 
     private static final String COMMAND_LANGUAGE = "language";
     private static final String COMMAND_LANGUAGE_SHORT = "lang";
-    private static final String COMMAND_LANGUAGE_STILL_SHORT = "lang";
+    private static final String COMMAND_LANGUAGE_STILL_SHORT = "l";
     private static final String CUSTOM_LANGUAGE = "ENG";
 
     private static final String COMMAND_LOGIN_PATTERN = "%s %s";
 
+//    @BeforeEach
+//    private void setUp() {
+//        appClientWriter = new AppClientWriter(serviceModel, fileReader, messageSource);
+//    }
+
     @Test
     void shouldReturnExpectedGreetingAfterLoginCommandEvaluated() {
-        appClientWriter = new AppClientWriter(serviceModel, fileReader, messageSource);
         String res;
         res = (String) shell.evaluate(() -> COMMAND_LANGUAGE_SHORT);
         assertThat(res).isEqualTo("Ваш выбор: RU");
