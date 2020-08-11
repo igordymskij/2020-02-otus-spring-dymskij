72d8325a26751112675a5486eaf8631488f625fb
Blicker777 <GitHub7>
2020-08-12 00:38:32 +0300

    Перенес аннотацию Transactional на методы

--- homework6-orm-jpa/src/main/java/com/education/spring/dao/CommentsDaoJpaImpl.java
+++ homework6-orm-jpa/src/main/java/com/education/spring/dao/CommentsDaoJpaImpl.java
@@ -14,13 +14,14 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
+
 @Repository
 public class CommentsDaoJpaImpl implements CommentsDao{
 
     @PersistenceContext
     private EntityManager em;
 
+    @Transactional
     @Override
     public Comment save(Comment comment) {
         if (comment.getId() <= 0) {
@@ -31,22 +32,26 @@ public class CommentsDaoJpaImpl implements CommentsDao{
         }
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<Comment> findById(int id) {
         return Optional.ofNullable(em.find(Comment.class, id));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Comment> findAll() {
         TypedQuery<Comment> query = em.createQuery("select c from Comment c", Comment.class);
         return query.getResultList();
     }
 
+    @Transactional
     @Override
     public Comment update(Comment comment) {
         return em.merge(comment);
     }
 
+    @Transactional
     @Override
     public void deleteById(int id) {
         Comment comment = findById(id).get();
